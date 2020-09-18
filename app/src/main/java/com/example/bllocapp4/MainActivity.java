73d07a2f97bac57c2533b18cedf977a8f67e167b
sean-hanlon <seanhanlon155@gmail.com>
2020-09-18 18:57:59 +0200

    updated main activity to handle intent from the other app

--- app/src/main/java/com/example/bllocapp4/MainActivity.java
+++ app/src/main/java/com/example/bllocapp4/MainActivity.java
@@ -2,13 +2,42 @@ package com.example.bllocapp4;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    private TextView txtView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        txtView = (TextView) findViewById(R.id.textView);
+
+        Intent intent = getIntent();
+        String action = intent.getAction();
+        String type = intent.getType();
+
+        if (Intent.ACTION_SEND.equals(action) && type != null) {
+            if("text/plain".equals(type)) {
+                handleSendText(intent);
+            }
+        }
+
+    }
+
+    private void handleSendText(Intent intent) {
+        // Get the text from intent
+        String sharedText = intent.getStringExtra(Intent.EXTRA_TEXT);
+        // When Text is not null
+        if (sharedText != null) {
+            // Append the notification from the other app to this text view
+            txtView.setText(sharedText);
+
+        }
     }
 }
